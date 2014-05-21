ed5a29b12d443f765f8142bc27b530cd70e30cc2
Paul Smith <paul0731@gmail.com>
2014-05-21 14:34:41 -0500

    fixed tests

--- src/test/groovy/com/eatmybits/floobits/prime/PrimeNumberTest.groovy
+++ src/test/groovy/com/eatmybits/floobits/prime/PrimeNumberTest.groovy
@@ -3,8 +3,45 @@ package com.eatmybits.floobits.prime
 import org.junit.Test
 
 class PrimeNumberTest {
+
     @Test
     void nullShouldNotBePrime(){
         assert !PrimeNumber.isPrime(null)
     }
+
+    @Test
+    void isPrimeShouldReturnFalseWithTheNumber0() {
+        assert !PrimeNumber.isPrime(0)
+    }
+
+    @Test
+    void isPrimeShouldReturnFalseWithTheNumber1() {
+        assert !PrimeNumber.isPrime(1)
+    }
+
+    @Test
+    void isPrimeShouldReturnTrueWithTheNumber2() {
+        assert PrimeNumber.isPrime(2)
+    }
+
+    @Test
+    void isPrimeShouldReturnTrueWithTheNumber3() {
+        assert PrimeNumber.isPrime(3)
+    }
+
+    @Test
+    void isPrimeShouldReturnFalseWithTheNumber4() {
+        assert !PrimeNumber.isPrime(4)
+    }
+
+    @Test
+    void isPrimeShouldReturnTrueWithTheNumber5() {
+        assert PrimeNumber.isPrime(5)
+    }
+
+    @Test
+    void isPrimeShouldReturnFalseWithTheNumber6() {
+        assert !PrimeNumber.isPrime(6)
+    }
+
 }
