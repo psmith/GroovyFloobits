e661b249f1a1314f91070035f87d9fac774aafe7
Paul Smith <paul0731@gmail.com>
2014-05-21 09:40:48 -0500

    setup

---
+++ src/main/groovy/1/Example.groovy
@@ -0,0 +1,15 @@
+//
+// Generated from archetype; please customize.
+//
+
+package 1
+
+/**
+ * Example Groovy class.
+ */
+class Example
+{
+    def show() {
+        println 'Hello World'
+    }
+}
